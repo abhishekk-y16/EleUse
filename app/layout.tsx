bb033e53fbcef9f3dde0ba2fc95249af0007f5b1
Abhishek Kumar Yadav <abhy4647@gmail.com>
2026-03-03 21:32:52 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "EleUse - Weather-Augmented Electricity Demand Analysis",
   description:
-    "Professional System for analyzing Weather-Electricity Correlations, Thermal sensitivity, and Demand forecasting using Statistical Modeling and Machine learning",
+    "Professional System for analyzing Weather-Electricity Correlations, Thermal Sensitivity, and Demand forecasting using Statistical Modeling and Machine learning",
   icons: {
     icon: [
       {
